45956d55023d5e407fbfec6d3bda468a2fbc52f3
ochestra365 <ochestra365@naver.com>
2021-02-22 11:02:58 +0900

    깃 커밋하는데 이그노어할 항목들 정리할 필요가 있다. 컴파일 에러 생김

--- chap03/HelloWolrdApp/Program.cs
+++ chap03/HelloWolrdApp/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace chap03App
+namespace HelloWolrdApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //주석입니다.
             Console.WriteLine("Hello, World");
         }
     }
---
+++ chap03/VariableTestApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int v1 = 30, v2 = 40;
+            int result = v1 + v2;
+            Console.WriteLine("결과는" + result);
+        }
+    }
+}
