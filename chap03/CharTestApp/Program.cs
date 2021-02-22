04f9b01e984e61d07044af65646532f2c442e943
ochestra365 <ochestra365@naver.com>
2021-02-22 12:33:28 +0900

    char 공부

---
+++ chap03/CharTestApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //문자-->키워드를 변수명으로 사용할 수 없다. 파란색은 전부 키워드이다. 예약어등으로 볼 수 있다.
+            char ch1 = 'T', ch2 = '\t', ch3 = '\n', ch4 = '0';
+            //escape문자 \t는 텝이다.\n은 출력 시 밑으로 떨어지게 하는 것이다. 콘솔에서 글자 표현할 때 필요한 것이다.
+            Console.WriteLine($"{ch1},{ch2},{ch3},{ch4}");
+            char ch5 = '\\';
+            Console.WriteLine($"{ch5}");
+            //원화표시하는 것은 ch5이다.
+
+            //문자열
+            string str1 = "\tHello, \nWorld!";
+           //Console.WriteLine(str1);
+            Console.WriteLine($"{str1}");
+
+            bool isCorrect = false;//true
+            if(isCorrect)
+            {
+                Console.WriteLine("참입니다.");
+            }
+            else
+            {
+                Console.WriteLine("거짓입니다.");
+            }
+            //화면에 찍힐 때, 글자로 바뀌어서 찍힌다는 것이다.
+            Console.WriteLine(isCorrect);
+        }
+    }
+}
--- chap03/VariableTestApp/Program.cs
+++ chap03/VariableTestApp/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace VariableTestApp
 {
@@ -10,9 +6,30 @@ namespace VariableTestApp
     {
         static void Main(string[] args)
         {
-            int v1 = 30, v2 = 40;
+            /*int v1 = 30, v2 = 40;
             int result = v1 + v2;
-            Console.WriteLine("결과는" + result);
+            Console.WriteLine("결과는" + result);*/
+            sbyte sbMinVal = sbyte.MinValue;
+            sbyte sbMaxVal = sbyte.MaxValue;
+            Console.WriteLine($"sbyte 최소, 최대값은 {sbMinVal},{sbMaxVal} 입니다.");
+            //byte
+            byte bMinVal = byte.MinValue, bMaxVal = byte.MaxValue;
+            Console.WriteLine($"byte 최소, 최대값은 {bMinVal},{bMaxVal} 입니다.");
+            //short
+            short shMinVal = short.MinValue,  shMaxval = short.MaxValue;
+            Console.WriteLine($"short 최소, 최대값은 {shMinVal},{shMaxval} 입니다.");
+            //ushort
+            ushort usMinVal=ushort.MinValue, usMaxVal = ushort.MaxValue;
+            Console.WriteLine($"usshort 최소, 최대값은 {usMinVal},{usMaxVal} 입니다.");
+            //int
+            int inMinVal = int.MinValue, inMaxVal = int.MaxValue;
+            Console.WriteLine($"int 최소, 최대값은 {inMinVal},{inMaxVal} 입니다.");
+            //long
+            long lnMinVal = long.MinValue, lnMaxVal = long.MaxValue;
+            Console.WriteLine($"long 최소, 최대값은 {lnMinVal},{lnMaxVal} 입니다.");
+            //ulong
+            ulong ulMinVal = ulong.MinValue, ulMaxVal = ulong.MaxValue;
+            Console.WriteLine($"long 최소, 최대값은 {ulMinVal},{ulMaxVal} 입니다.");
         }
     }
 }
