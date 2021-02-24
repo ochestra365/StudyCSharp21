278b1dfddac3854081383ac81a80c28d8b50f453
ochestra365 <ochestra365@naver.com>
2021-02-24 17:00:38 +0900

    정리안 커밋

--- chaop07/AccessModifierTestApp/MainApp.cs
+++ chaop07/AccessModifierTestApp/MainApp.cs
@@ -6,15 +6,15 @@ using System.Threading.Tasks;
 
 namespace AccessModifierTestApp
 {
-    class Boiler
+    class Boiler//보일러의 가동원리를 모듈화한 것이다.
     {
-        private int temp=5;//물온도
+        internal int temp=5;//물온도, 나중에 미니프로젝트에서 실측한 정보가 멤버변수로 설정되는 것을 유추할 수 있다.//public, internal, protected
 
         public void SetTemp(int temp)
         {
             if (temp < 30 || temp > 60)
             {
-                Console.WriteLine("물의온도가 높아. 59로 ㄱㄱ");
+                Console.WriteLine("물의온도가 불안정해. 59로 ㄱㄱ");
                 this.temp = 59;
                 return;
             }
@@ -25,7 +25,7 @@ namespace AccessModifierTestApp
             this.temp = temp;
         }
 
-        public int GetTemp()
+        public int GetTemp()// 물온도를 사용자가 건들지 못하게 메서드화함. 은닉성/캡슐화/보안강화/해킹방지.
         {
             return this.temp;
         }
@@ -68,3 +68,8 @@ namespace AccessModifierTestApp
 }
 //모든 속성을 public으로 하니까 원하는 대로 정보를 변환할 수 있다. 해킹의 위험성이 있다. 정보 은닉(캡슐화)이다.
 //사용자가 임의의 값을 넣어서 실제 현상에서 일어날 수 있는 참사를 막을 수 있다.
+//메인 클래스 메인메서드는 다른 클래스를 매개변수(파라미터)로써 사용한다고 볼 수 있는가? 그렇다고 볼 수 있을 것이다.(물어보니까 아니다-- 그거는 컴파일러에서 설정된 것이다. 실행과 부모 조상 릴레이션은 다른 것이다.)
+//클래스의 생성은 부모자식의 상속관계가 아니라 사용관계(실행)이다.
+//internal은 같은 어쎔블리에 적용이 가능하다.
+//아무 값이나 걸 수 없게, 도메인을 한정짓기 위해, settemp public을 해준다. 하나의 안전장치를 줄 수 있는 것이다. 부모에게 물려받은 상속 조건들은 자식에서 사용할 수 있다는 것이
+//protected이다.
