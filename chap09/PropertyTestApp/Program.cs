62f380a2619f7a1cfbaebedf2d683c94640ce7c6
ochestra365 <ochestra365@naver.com>
2021-02-25 17:08:17 +0900

    프로퍼티 깨닳음 커밋

--- chap09/PropertyTestApp/Program.cs
+++ chap09/PropertyTestApp/Program.cs
@@ -16,10 +16,12 @@ namespace PropertyTestApp
         //사용자가 건드려선 안되는 정보를 보호해주기 위해서 private를 써준다.
         private string name;
         private DateTime birthday;
-        public string Name
+        public string Name//프로퍼티는 메서드를 하나의 인스턴스로 여기는 것이다.
         {
-            get { return this.name; }
-            set { this.name = value; }//-->파라미터로써 name을 받아서 쓰는 메서드와는 달리 이번에는 파라미터가 없다. 그래서 value를 쓴다. (할당할 파라미터가 없다.)
+            get { return this.name; }//리턴은 결과값을 반환하는 것이기 때문에 파라미터가 따로 필요없다. 왜냐하면 this를 통해 멤버변수를 쓰겠다는 거니까.
+            set { this.name = value; }//그러나 세팅은 다르다. 세팅은 결과값을 반환하지 않는 void가 숨겨져 있다. 그런데 메서드가 아니기 때문에 데이터 타입을 적을 수 없으니
+            // value로 놓는 것이다. value는 컴파일러에서 특정 로직을 타고 합리적인 데이터타입을 선언하기 때문이다.
+            //-->파라미터로써 name을 받아서 쓰는 메서드와는 달리 이번에는 파라미터가 없다. 그래서 value를 쓴다. (할당할 파라미터가 없다.)
         }
 
         public DateTime Birthday
@@ -46,6 +48,7 @@ namespace PropertyTestApp
         {
             this.birthday = birthday;//경로 호출하여 할당. 이름을 같게 쓰는 것은 속성 값을 그대로 가져오기 위함인데, 보안이 걸려 있어서 해주는 것이다.(은닉성, 캡슐화)
         }
+        //set 메서드에 void를 끼워주는 이유는 사용자가 필요할 때, 자료를 꺼내 쓰기 위해서 결과값을 반환시키지 않는 의도인 것이다.
     }
 
     class Program
@@ -70,6 +73,7 @@ namespace PropertyTestApp
             //프로퍼티를 쓰면 멤버변수에 접근한 것처럼 한다. 프로퍼티가 훨씬 코딩량이 줄고 간결하게 쓸 수 있다.
             //분리된 메서드를 가지고 일처리를 하는 것은 비효율적이기 때문에 프로퍼티로 관리하는 것이 간결하고 정확함.
             //프로퍼티를 쓰면 메서드를 호출하는 것이 아니라 하나의 변수처럼 관리할 수 있음.
+            //이해하기 어려운 것은 추상성이 높은 학문이기 때문이다. 추상성을 추리하여 자기의 학문으로 만드는 것은 개발자의 노력에 달려 있다.
         }
     }
 }
