2f8a72702b4eaa2ce9056dad257cfffe07d40d00
ochestra365 <ochestra365@naver.com>
2021-03-02 10:35:04 +0900

    커밋후 동기화

---
+++ chap10/ArrayListApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList array = new ArrayList(new[] { 80,74,81,90,34});
+         /*   array.Add(80);
+            array.Add(74);
+            array.Add(81);
+            array.Add(90);
+            array.Add(34);*/
+
+            var loc = array.IndexOf(81);
+            array.Insert(loc, 50);
+
+            Console.WriteLine("81위치에 50 추가.");
+            foreach (var item in array)//배열은 foreach
+            {
+                Console.WriteLine($"{item}");
+            }
+            loc = array.IndexOf(90);
+            array.RemoveAt(4);//90 삭제-->위치에 있는 값을 지우는 것이다.
+
+            foreach (var item in array)
+            {
+                Console.WriteLine($"{item}");
+            }
+
+            array.Sort();
+            foreach (var item in array)
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+    }
+}
---
+++ chap10/HashTableApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace HashTableApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HashTable 예제");
+
+            Hashtable ht = new Hashtable();
+            ht["일"] = "One";
+            ht["이"] = "Two";
+            ht["삼"] = "Three";
+            ht["사"] = "Four";//foreach로 바로 안됨.
+
+            Console.WriteLine(ht["일"]);//One
+
+            foreach (DictionaryEntry item in ht)
+            {
+                Console.WriteLine($"{item.Key} : {item.Value}");//키 값에 상응하는 값을 출력한다는 콘솔 창 의미이다.
+            }
+
+            int[] arr = { 123, 456, 789 };
+            ArrayList list = new ArrayList(arr);
+            ArrayList list2 = new ArrayList(new int[] { 123, 456, 789 });
+            ArrayList list3 = new ArrayList() {123,456,789 };
+        }
+    }
+}
