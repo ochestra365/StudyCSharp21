e5ac49f894e33fd3b07f7fbe2dca285f262e5d82
ochestra365 <ochestra365@naver.com>
2021-03-04 16:18:32 +0900

    주소 관리자 디버깅

--- chap99/AddressBookApp/AddressManager.cs
+++ chap99/AddressBookApp/AddressManager.cs
@@ -24,7 +24,7 @@ namespace AddressBookApp
         }//기본 화면창이라 볼 필요가 없다.
         public int SelectMenu()
         {
-            string input = Console.ReadLine();//input(문자열이라는 속성을 선언한다.)이라는 인스턴스에 콘솔창에서 라인을 적을 수 있는 권한을 할당한다.
+            /*string input = Console.ReadLine();//input(문자열이라는 속성을 선언한다.)이라는 인스턴스에 콘솔창에서 라인을 적을 수 있는 권한을 할당한다.
             int menuNum = 0;//menuNum이라는 int 속성을 할당하고 이것의 값은 0이라고 할당한다.
             int.TryParse(input, out menuNum);//변환하는 것이다. input은 문자열이다. 이것을 숫자열로 변환하고 출력반환한다.
             Console.WriteLine($"선택된 메뉴번호 : {menuNum}");//사용자가 적을 수 있는 콘솔 화면을 찍고
@@ -33,11 +33,19 @@ namespace AddressBookApp
                 menuNum = 0;
             }
 
-            return SelectMenu();
+            return SelectMenu();*/
             // Console.WriteLine($"선택된 메뉴 : {menuNum}");
             // 다른 메서드들은 기능을 구현하거나 저장하는 list형식이 걸려 있지만 메뉴를 선택하는 것은 다르다.
             // 사용자가 해당 숫자를 클릭하고 엔터치면 그 값을 호출한 곳으로 연산결과를 돌려주지 않으면 작동되지 않는 것이다.
             // 메서드는 연산만 담당하지 값을 호출하는 기능을 하지 않는다.
+
+            // 메뉴번호 입력
+            Console.Write("메뉴를 선택하세요 >>> ");
+            string input = Console.ReadLine();
+            int.TryParse(input, out int result);
+            if (!(result > 0 && result < 7)) result = 0;
+
+            return result;
         }
         public void InputAddress()
         {
@@ -92,7 +100,7 @@ namespace AddressBookApp
 
         public void DeleteAddress()
         {
-            Console.WriteLine("주소삭제");
+            /*C*//*//**onsole.WriteLine("주소삭제");
             Console.WriteLine("-------------------------------------");
             Console.Write("이름 입력 : ");
             string name = Console.ReadLine();
@@ -115,14 +123,48 @@ namespace AddressBookApp
                         listAddress.RemoveAt(idx);
                     break;//foreach 빠져나감.
                 }
-                idx++;
-            }
+                idx++;*/
+            /*}
             if (isFind == false)
             {
                 Console.WriteLine("검색결과가 없습니다.");
             }
-            Console.ReadLine();//화면멈춤
+            Console.ReadLine();//화면멈춤*/
+        Console.WriteLine("주소삭제");
+            Console.WriteLine("----------------------------------------");
+            Console.Write("이름 입력 : ");
+            string name = Console.ReadLine();
+    int idx = 0;
+    bool isFind = false;
+            foreach (var item in listAddress)
+            {
+                if (item.Name == name)
+                {
+                    isFind = true;
+                    Console.WriteLine($"[{idx}]------------------------------------");
+                    Console.WriteLine($"이름 : {item.Name}");
+                    Console.WriteLine($"전화 : {item.Phone}");
+                    Console.WriteLine($"주소 : {item.Address}");
+                    Console.WriteLine("----------------------------------------");
+                    Console.Write("삭제하시겠습니까? [y/n]");
+                    string answer = Console.ReadLine(); // y/n
+                    if (answer.ToUpper() == "Y")
+                    {
+                        listAddress.RemoveAt(idx);
+                        Console.WriteLine("삭제했습니다.");
+                    }
 
+break; // foreach빠져나감
+                }
+                idx++;
+            }
+
+            if (isFind == false)
+{
+    Console.WriteLine("검색결과가 없습니다.");
+}
+
+Console.ReadLine(); // 화면멈춤
         }
         public void UpdateAddress()
         {
